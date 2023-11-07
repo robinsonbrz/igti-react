be4e11e5908b87a0591ea24e40d72672a8063f31
Robinson Enedino <robinsonbrz@gmail.com>
2023-11-06 23:36:53 -0300

    chore: input mod 2 aula 2.5

--- mod02/proj-base-1.5/src/App.js
+++ mod02/proj-base-1.5/src/App.js
@@ -5,13 +5,13 @@ import Main from "./components/Main";
 export default function App() {
   console.log('Teste no console do navegador');
 
-  // const name = 'Robinson';
 
   return (
     <>
       <Header size="large">Componente Header - projeto react-hello large</Header>
       <Header >Componente Header - projeto react-hello normal</Header>
       <Main>
+        <input className="border" type="text" />
         <p>
           Olá Robinson! Quantidade de caracteres em nome 8 
         </p>
